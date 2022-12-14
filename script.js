f4108a93846730ecd3131e218611cb9ebaa59656
danielbrodd <daniel.brodd@gmail.com>
2022-12-14 22:37:53 +0100

    Get computers choice

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function randomInt (){
     return rInt;
 }
 
+function getComputerChoice() {
+    let rInt = parseInt(randomInt());
+    if (rInt == 1) {
+        return "Rock";
+    } else if (rInt == 2) {
+        return "Paper";
+    } else if (rInt == 3) {
+        return "Scissor";
+    }else return "you chose chaos!";
+}
+
