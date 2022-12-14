64d3f509adbe16f197a52b80363867f9e39eb6da
danielbrodd <daniel.brodd@gmail.com>
2022-12-14 23:25:00 +0100

    Add 1 round play function

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
+// random integer between 1-3
+
 function randomInt (){ 
     let rInt = Math.floor(Math.random()* (3-1+1)+1);
     return rInt;
 }
-
+// random computer choice based on randomInt result
 function getComputerChoice() {
     let rInt = parseInt(randomInt());
+  
     if (rInt == 1) {
         return "Rock";
     } else if (rInt == 2) {
@@ -15,3 +18,19 @@ function getComputerChoice() {
     }else return "you chose chaos!";
 }
 
+// game
+function playRound (playerSelection = prompt().toUpperCase(), computerSelection = getComputerChoice()) {
+    let message = ''
+    
+    if (playerSelection == "ROCK" && computerSelection != "Rock") {
+        playerSelection == "ROCK" && computerSelection == "Paper" ? message = "You loose" : message = "You Win";
+    } else if (playerSelection == "PAPER" && computerSelection != "Paper") {
+        playerSelection == "PAPER" && computerSelection == "Scissor" ? message = "You loose": message = "You Win"
+    } else if (playerSelection == "SCISSOR" && computerSelection != "Scissor") {
+        playerSelection == "SCISSOR" && computerSelection == "Rock" ? message = "You loose": message = "You win";
+    }
+    else message = "DRAW!" ;
+    console.log(computerSelection);
+    return message ;
+    
+}
